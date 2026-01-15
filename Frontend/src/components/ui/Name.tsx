1f22db95045999005aa8438e57f53c2019d091cc
Pedrolamon <plamon48@gmail.com>
2026-01-15 14:46:55 -0300

    component name default

---
+++ Frontend/src/components/ui/Name.tsx
@@ -0,0 +1,17 @@
+export default function Logo() {
+    return (
+      <div className="flex items-center ">
+        <span className="font-black text-2xl tracking-tighter uppercase italic">
+          <span className="text-emerald-500">W</span>
+          <span className="text-amber-400">O</span>
+          <span className="text-slate-400">R</span>
+          <span className="text-emerald-500">D</span>
+        </span>
+  
+        <span className=" font-black text-2xl tracking-tighter uppercase italic text-(--button-color)">
+          Game
+        </span>
+      </div>
+    );
+  }
+  
