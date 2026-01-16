32ef766af925115a8e9442daf42171fa1d002577
Pedrolamon <plamon48@gmail.com>
2026-01-16 09:20:05 -0300

    adding size to the logo

--- Frontend/src/components/ui/Name.tsx
+++ Frontend/src/components/ui/Name.tsx
@@ -1,17 +1,20 @@
-export default function Logo() {
+interface LogoProps {
+    size?: string; 
+  }
+  
+  export default function Logo({ size = "text-2xl" }: LogoProps) {
     return (
-      <div className="flex items-center ">
-        <span className="font-black text-2xl tracking-tighter uppercase italic">
+      <div className="flex items-center">
+        <span className={`font-black ${size} tracking-tighter uppercase italic`}>
           <span className="text-emerald-500">W</span>
           <span className="text-amber-400">O</span>
           <span className="text-slate-400">R</span>
           <span className="text-emerald-500">D</span>
         </span>
   
-        <span className=" font-black text-2xl tracking-tighter uppercase italic text-(--button-color)">
+        <span className={`font-black ${size} tracking-tighter uppercase italic text-(--button-color)`}>
           Game
         </span>
       </div>
     );
   }
-  
