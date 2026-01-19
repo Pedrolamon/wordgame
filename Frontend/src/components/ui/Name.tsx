f8dd012a5d2d51101246af4fd0ad6ac9b2b49719
Pedrolamon <plamon48@gmail.com>
2026-01-19 13:55:36 -0300

    adding the logo link function

--- Frontend/src/components/ui/Name.tsx
+++ Frontend/src/components/ui/Name.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 interface LogoProps {
-    size?: string; 
-  }
-  
-  export default function Logo({ size = "text-2xl" }: LogoProps) {
-    return (
+  size?: string;
+}
+
+export default function Logo({ size = "text-2xl" }: LogoProps) {
+  return (
+    <Link to="/" className="hover:opacity-80 transition-opacity">
       <div className="flex items-center">
         <span className={`font-black ${size} tracking-tighter uppercase italic`}>
           <span className="text-emerald-500">W</span>
@@ -11,10 +14,11 @@ interface LogoProps {
           <span className="text-slate-400">R</span>
           <span className="text-emerald-500">D</span>
         </span>
-  
+
         <span className={`font-black ${size} tracking-tighter uppercase italic text-(--button-color)`}>
           Game
         </span>
       </div>
-    );
-  }
+    </Link>
+  );
+}
